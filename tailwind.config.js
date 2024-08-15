e86c213c21d7e83e659a02d488110c9150d2d2e6
dreammehedi <dreammehedihassan@gmail.com>
2024-08-15 14:04:48 +0600

    Font Family and Some Main Color Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    fontFamily: {
+      lato: ["Lato", "sans-serif"],
+      "fira-sans": ["Fira Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#2B6CB0",
+        secondary: "#319795",
+        background: "#F7FAFC",
+        dark: "#2D3748",
+        accent: "#E53E3E",
+        success: "#38A169",
+        warning: "#DD6B20",
+        gray: "#CBD5E0",
+      },
+    },
   },
   plugins: [],
 };
