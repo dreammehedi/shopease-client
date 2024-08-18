0de69419ea848b158bf1bb6fe658218ea9fa2110
dreammehedi <dreammehedihassan@gmail.com>
2024-08-18 14:49:15 +0600

    useGetProducts api call mistake solve and final deploy complete

--- src/hooks/useGetProducts.jsx
+++ src/hooks/useGetProducts.jsx
@@ -1,19 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import useAxiosPublic from "../axios/useAxiosPublic";
 
 function useGetProducts(activePage, searchProduct, sortedBy, filter) {
+  const axiosPublic = useAxiosPublic();
+
   const { data, refetch: allProductsRefetch } = useQuery({
     queryKey: ["allProducts", activePage, searchProduct, sortedBy, filter],
     queryFn: async () => {
-      const fetchProducts = await axios.post(
-        `http://localhost:5000/api/products`,
-        {
-          activePage,
-          searchProduct,
-          sortedBy,
-          filter,
-        }
-      );
+      const fetchProducts = await axiosPublic.post(`/products`, {
+        activePage,
+        searchProduct,
+        sortedBy,
+        filter,
+      });
       const resData = await fetchProducts.data;
       return resData;
     },
