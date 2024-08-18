3b1a11e11d5840568688caf095b81cb11feddaad
dreammehedi <dreammehedihassan@gmail.com>
2024-08-18 14:35:18 +0600

    Server URL Change

--- src/axios/useAxiosPublic.jsx
+++ src/axios/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "https://shopeasy-new-server.vercel.app/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shopeasy-new-server.vercel.app/api",
+  // baseURL: "http://localhost:5000/api",
 });
 function useAxiosPublic() {
   return axiosPublic;
